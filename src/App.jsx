2ec52a762854d1bb25f945a2c57fb170758ca474
Paweł <55333847+zxasc@users.noreply.github.com>
2023-10-10 00:52:12 +0200

    add dynamic background colors

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,64 @@ import { useState, useRef } from 'react'
 import CurrentWeather from './components/CurrentWeather';
 import ForecastWeather from './components/ForecatsWeather';
 import placeholder from './placeholder.json'
-import './styles/styles.css'
+import './styles/styles.scss'
+//import variables from './styles/variables.module.scss'
 
 export default function App() {
   const [weatherInfo, setWeatherInfo] = useState(placeholder);
+  const [isLoading, setIsLoading] = useState(false);
   const searchBar = useRef();
+
+  const selectColorScheme = (data) => {
+    const backgroundColors = [
+      "002357", // 0
+      "033770", // 1
+      "01356e", // 2
+      "00406c", // 3
+      "024270", // 4
+      "01628d", // 5
+      "046f9b", // 6
+      "119ebc", // 7
+      "6ecfc8", // 8
+      "71d3cb", // 9
+      "a8ddbf", // 10
+      "e8eb90", // 11
+      "fede67", // 12
+      "ffca62", // 13
+      "fdaf5c", // 14
+      "f69f50", // 15
+      "f5866b", // 16
+      "da6d82", // 17
+      "a64e88", // 18
+      "6c3384", // 19
+      "29156a", // 20
+      "1b2763", // 21
+      "050d3e", // 22
+      "040c3d" // 23
+    ]
+    const currentTime = Number(data.current.time.substring(0,2));
+    document.documentElement.style.setProperty('--clr-backround', `#${backgroundColors[currentTime]}`)
+  }
 
   const handleForm = async (e) => {
     e.preventDefault();
-
+    setIsLoading(true);
+    
     try {
       const response = await fetch(`https://weather-app-back-end-0lvz.onrender.com/${searchBar.current.value}`, { mode: 'cors' });
-      const data = await response.json();
-      setWeatherInfo(data);
+      
+      if(response.ok) {
+        const data = await response.json();
+        selectColorScheme(data);
+        setWeatherInfo(data);
+      } else {
+        console.log("Invalid city search")
+      }
+      
     } catch(err) {
-      console.error(err);
+      console.log(err)
     } finally {
-      console.log(weatherInfo)
+      setIsLoading(false);
     }
   };
 
